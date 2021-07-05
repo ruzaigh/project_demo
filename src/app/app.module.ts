8826ec7ffc8e8f916a6548d5a5f18f827c774a05
ruzaigh <ruzaigh.kalam2@gmail.com>
2021-07-05 15:56:16 +0200

    old

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import { AuthComponent } from 'src/auth/auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // AuthComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/auth/auth/auth.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent implements OnInit {
+   isLoginMode = true;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onSwitchMode( ){
+    this.isLoginMode = !this.isLoginMode;
+  }
+}
